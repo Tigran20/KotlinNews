58d2c8b2a6e10b693c371e3cc8ae2301e4b11e57
Tigran20 <Podliza122@bk.ru>
2018-06-06 15:31:39 +0300

    добавил Picasso и Intent при клике на элемент

--- app/src/main/java/com/alextroy/news22/adapter/NewsAdapter.kt
+++ app/src/main/java/com/alextroy/news22/adapter/NewsAdapter.kt
@@ -2,6 +2,7 @@ package com.alextroy.news22.adapter
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.alextroy.news22.R
 import com.alextroy.news22.model.Result
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.news_item.view.*
 
 class NewsAdapter(private val context: Context) : RecyclerView.Adapter<NewsAdapter.NewsHolder>() {
@@ -27,16 +29,18 @@ class NewsAdapter(private val context: Context) : RecyclerView.Adapter<NewsAdapt
         return news.size
     }
 
-    class NewsHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+    class NewsHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bindItems(news: Result) {
+            Picasso.get().load(news.fields.thumbnail).into(itemView.news_image)
             itemView.news_title.text = news.webTitle
             itemView.news_description.text = news.fields.trailText
-            itemView.setOnClickListener(this)
-        }
-
-        override fun onClick(v: View?) {
-            Log.d("RecyclerView", "CLICK!")
+            itemView.setOnClickListener {
+                Log.d("RecyclerView", "CLICK!")
+                val newsUri = Uri.parse(news.webUrl)
+                val intents = Intent(Intent.ACTION_VIEW, newsUri)
+                itemView.context.startActivity(intents)
+            }
         }
     }
 
